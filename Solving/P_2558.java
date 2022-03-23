80aabd4c5d236fe564d09d95cf146509b20116fc
gkddn97 <bullie_hu@naver.com>
2022-03-23 23:27:25 +0900

    테스트용 A+B 완

---
+++ Solving/P_2558.java
@@ -0,0 +1,17 @@
+package Solving;
+// A+B - 2 2558
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_2558 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+
+        System.out.println(num1 + num2);
+    }
+    
+}
