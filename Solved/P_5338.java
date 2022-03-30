4080e373a548a07512b8f1659b6084b21f00fa35
gkddn97 <bullie_hu@naver.com>
2022-03-30 22:14:30 +0900

    마이크로소프트 로고 완

--- Solved/P_5338.java
+++ Solved/P_5338.java
@@ -1,5 +1,5 @@
-pakage Solved;
-// 마이크로소프트 로고
+package Solved;
+// 마이크로소프트 로고 5338
 public class P_5338 {
     public static void main(String[] args) {
         System.out.println("       _.-;;-._");
