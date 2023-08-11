f6a61e318debd16e6c9c8c6756e7ba375d2b65d4
Bulliiee <61936103+Bulliiee@users.noreply.github.com>
2023-08-11 20:45:47 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 500 ms, Memory: 294156 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		// 입력 N, K
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		// 1 ~ N까지 담을 큐
+		Queue<Integer> queue = new ArrayDeque<Integer>();
+		for(int i = 1; i <= N; i++) {	// 초기에 1 ~ N까지 담아준다.
+			queue.offer(i);
+		}
+		
+		int[] result = new int[N];	// 결과 담을 배열
+		int resIdx = 0;	// result 저장용 인덱스
+		int count = 0;	// K 도달 여부 확인 카운트
+		while(!queue.isEmpty()) {	// 큐에 값이 있는동안은 계속 연산
+			int temp = queue.poll();
+			count++;
+			
+			if(count != K) {
+				queue.offer(temp);
+			} else {	// 카운트 도달시 결과 배열에 넣기
+				result[resIdx++] = temp;
+				count = 0;
+			}
+		}
+		
+		// 결과 출력
+		sb.append("<");
+		for(int i = 0; i < N-1; i++) {
+			sb.append(result[i] + ", ");
+		}
+		sb.append(result[N-1] + ">");
+		
+		System.out.print(sb.toString());
+	}
+
+}
